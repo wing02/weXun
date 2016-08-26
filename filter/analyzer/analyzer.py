8d7d2493e855e3ed650c83ecb2814ceb359436a0
wing02 <531857684@qq.com>
2016-08-26 13:55:43 +0800

    done TM1

--- filter/analyzer/analyzer.py
+++ filter/analyzer/analyzer.py
@@ -1,32 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cPickle
+import pdb
 
 class Analyzer:
-    def drawROC(self,results):
+    def drawROC(self,results,flags):
+        #pdb.set_trace()
         step=0.01
         rate=0
         TPRs=[]
         FPRs=[]
         for rate in np.linspace(0,1,101):
             TP=FP=TN=FN=0
-            for result in resutls:
-                if result[0]>rate and result[1]==1:
+            for result,flag in zip(results,flags):
+                if result>rate and flag==1:
                     TP+=1
-                elif result[0]<=rate and result[1]==1:
+                elif result<=rate and flag==1:
                     FN+=1
-                elif result[0]>rate and result[1]==0:
+                elif result>rate and flag==0:
                     FP+=1
-                elif result[0]<rate and result[1]==0:
+                elif result<=rate and flag==0:
                     TN+=1
+            #print (TP,TN,FP,FN)
             TPRs.append(float(TP)/(TP+FN))
             FPRs.append(float(FP)/(FP+TN))
         print ('Top 90 :',TPRs[90],FPRs[90])
         plt.plot(FPRs,TPRs)
+        plt.grid()
         plt.show()
 
 if __name__=='__main__':
-    resultPath='data/result.pkl'
-    results,flags=cPickele.load(open(resultPath))
+    resultPath='filter/sexyImgFilter/data/result.pkl'
+    results,flags=cPickle.load(open(resultPath))
     results=map(lambda x:x[1],results)
     analyzer=Analyzer()
-    analyzer.drawROC((results,flags))
+    analyzer.drawROC(results,flags)
--- filter/sexyImgFilter/test.py
+++ filter/sexyImgFilter/test.py
@@ -9,15 +9,17 @@ import lmdb
 import scipy.io as sio
 import io
 import cPickle
+import pdb
+from scipy.special import expit
 
 sys.path.append('/home/lab401/Project/caffe/python')
 import caffe 
 
 class Test:
     def __init__(self):
-        model_def = 'data/train.prototxt'
-        model_weights = 'data/snapshot/_iter_10000.caffemodel'
-        meanFile='data/mean/trainMean.binaryproto'
+        model_def = 'filter/sexyImgFilter/data/deploy.prototxt'
+        model_weights = 'filter/sexyImgFilter/data/snapshot/_iter_10000.caffemodel'
+        meanFile='filter/sexyImgFilter/data/mean/trainMean.binaryproto'
         caffe.set_mode_cpu()
         caffe.set_device(0)
         self.net = caffe.Net(model_def,model_weights,caffe.TEST) 
@@ -26,36 +28,41 @@ class Test:
         blob.ParseFromString(data)
         mu=np.array(caffe.io.blobproto_to_array(blob))
         self.transformer = None
-        self.transformer = caffe.io.Transformer({'data': self.net.blobs['data1'].data.shape})
+        self.transformer = caffe.io.Transformer({'data': self.net.blobs['data'].data.shape})
         self.transformer.set_transpose('data', (2,0,1))  # move image channels to outermost dimension
         self.transformer.set_mean('data', mu[0]*0.00390625)            # subtract the dataset-mean value in each channel
         self.transformer.set_channel_swap('data', (2,1,0)) # swap channels from RGB to BGR
 
-    def testImg(self,data)
+    def testImg(self,data):
         image=self.loadImgFromBinary(data)
         transformed_image = self.transformer.preprocess('data', image)
-        self.net.blobs['data1'].data[...] = transformed_image
+        self.net.blobs['data'].data[...] = transformed_image
+        #pdb.set_trace()
         output = self.net.forward()
         result=self.net.blobs['ip'].data[0]
-        return result
+        return expit(result)
 
     def loadImgFromBinary(self,data):
         srcPath=io.BytesIO(data)
         image = caffe.io.load_image(srcPath)
         return image
+
     
 if __name__=="__main__":
-    dataPath='data/test.txt'
-    desPath='data/result.pkl'
+    prePath='filter/sexyImgFilter/'
+    dataPath='filter/sexyImgFilter/data/test.txt'
+    desPath='filter/sexyImgFilter/data/result.pkl'
     results=[]
     flags=[]
     test=Test()
     with open(dataPath) as f:
         for line in f:
             tmp=line.split(' ')
-            path=tmp[0]
+            path=prePath+tmp[0]
             flags.append(int(tmp[1]))
             with open(path) as fImg:
                 result=test.testImg(fImg.read())
+                #print (path,result)
                 results.append(result)
+                print (result,tmp[1])
     cPickle.dump((results,flags),open(desPath,'w'))
